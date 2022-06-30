598e060ccc8edab9fa8f0eb626e63aa429aa079b
tracer-nju <tracer@nju.edu.cn>
2022-06-30 23:18:30 +0800

    Linux LAPTOP-79644HQ4 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:18:30 up 15 min,  0 users,  load average: 0.25, 0.17, 0.08

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -182,7 +182,6 @@ void syscall_fork(Context *ctx) {
         // phypg_t *page = mytask()->pps[i];
         phypg_t* page = alloc_page(page_list, mytask()->as.pgsize);
         memcpy(page->pa, mytask()->pps[i]->pa, mytask()->as.pgsize);
-        printf("%d parent=%d, %p -> %p\n", mytask()->pid, mytask()->parent->pid, va, page->pa);
         assert((uintptr_t)page->pa == ROUNDDOWN(page->pa,mytask()->as.pgsize));
         map(&child->as, va, page->pa, MMAP_READ | MMAP_WRITE);
         // map(&mytask()->as, va, page->pa, MMAP_NONE);
