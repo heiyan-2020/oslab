8436065505bc34b698256d293abd8e0470fa1ba9
tracer-nju <tracer@nju.edu.cn>
2022-06-30 23:11:58 +0800

    Linux LAPTOP-79644HQ4 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:11:58 up 8 min,  0 users,  load average: 0.10, 0.11, 0.04

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -146,7 +146,6 @@ Context *schedule(Event ev, Context *context) {
     panic_on(ienabled(), "\n");
 
     spin_lock(&schedule_lk);
-    printf("first\n");
     task_t *cur = mytask();
 
     task_t *round_begin = cur == idles_[mycpu()->cpuno] ? tlist_->head : cur->next;
